4da640416800b9acb5d79fae30301d57718a9c23
SEIF <mohammedseifaldin@gmail.com>
2024-09-17 18:30:10 +0300

    adding core

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,38 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+import 'core/init_servicess.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initServicess();
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('ar')],
+      path: 'assets/languages', // <-- change the path of the translation files
+      fallbackLocale: const Locale('en'),
+      child: const MyApp(),
+    ),
+  );
+}
+
+final navKey = GlobalKey<NavigatorState>();
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Open Fashion',
+      debugShowCheckedModeBanner: false,
+      navigatorKey: navKey,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      home: const MainApp(),
+    );
+  }
 }
 
 class MainApp extends StatelessWidget {
